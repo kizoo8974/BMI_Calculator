6bff4ff3a836ad6a2c6f234ae0595298a642e39c
Joon Kim <kizoo8974@gamil.com>
2023-06-13 08:51:31 +0900

    project completed.

--- main.js
+++ main.js
@@ -7,6 +7,33 @@ function onSubmit(event) {
         alert("Pease, enter a proper number.")
         return;
     }
+    const bmi = w / (h*h)
 
-   
+    console.log(bmi.toFixed(2))
+
+    const res = document.getElementById("res")
+
+    res.style.display = "block"
+
+    document.getElementById("bmi").innerText = bmi.toFixed(2)
+    document.getElementById("meter").value = bmi
+
+let state = "normal"
+let common = true;
+
+    if(bmi < 18.5) {
+        state = "underweight"
+        common = false;
+    }
+        
+    if(bmi >= 25) {
+        state = "overweight"
+        common = false;
+    }
+        
+
+    const stateElement = document.getElementById("state");
+    stateElement.innerText = state
+    stateElement.style.color = common ? "#29EF21" : "#FF3A3A"
 }
+
